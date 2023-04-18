7fc02b689b17797119b929d9db3ef900a4885869
Thorsten Neff <Thorsten.Neff@rtl-extern.de>
2023-04-18 18:46:58 +0200

    Deps for ShowKase testing

--- ui/src/test/java/com/example/ui/MyPaparazziTest.kt
+++ ui/src/test/java/com/example/ui/MyPaparazziTest.kt
@@ -1,23 +1,24 @@
 package com.example.ui
 
-import app.cash.paparazzi.DeviceConfig
-import app.cash.paparazzi.Paparazzi
-import com.android.ide.common.rendering.api.SessionParams
-import org.junit.Rule
-import org.junit.Test
+import com.airbnb.android.showkase.annotation.ShowkaseScreenshot
+import com.airbnb.android.showkase.screenshot.testing.paparazzi.PaparazziShowkaseScreenshotTest
 
-
-class MyPaparazziTest {
-    @get:Rule
-    val paparazzi = Paparazzi(
-        deviceConfig = DeviceConfig.PIXEL_6_PRO.copy(softButtons = false),
-        renderingMode = SessionParams.RenderingMode.SHRINK
-    )
-
-    @Test
-    fun `test homescreen`() {
-        paparazzi.snapshot {
-            HomeScreen()
-        }
-    }
+@ShowkaseScreenshot(rootShowkaseClass = RootModule::class)
+abstract class MyPaparazziShowkaseScreenshotTest: PaparazziShowkaseScreenshotTest {
+    companion object: PaparazziShowkaseScreenshotTest.CompanionObject
 }
+
+//class MyPaparazziTest {
+//    @get:Rule
+//    val paparazzi = Paparazzi(
+//        deviceConfig = DeviceConfig.PIXEL_6_PRO.copy(softButtons = false),
+//        renderingMode = SessionParams.RenderingMode.SHRINK
+//    )
+//
+//    @Test
+//    fun `test homescreen`() {
+//        paparazzi.snapshot {
+//            HomeScreen()
+//        }
+//    }
+//}
